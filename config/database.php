8c34ce2296549ab5bd4c62063526f77136f82ef4
Ponco Herbowo <ponco.94@gmail.com>
2019-08-12 14:54:33 +0700

    added postgres database config

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
+$DATABASE_URL=parse_url('postgres://tyjtdmhtrnkaxk:bc98933c86ca605c26ab78262d80855863f8a612ca68fafe93639574042ba3ac@ec2-54-235-92-43.compute-1.amazonaws.com:5432/dapj638jgcljka');
 
 return [
-
     /*
     |--------------------------------------------------------------------------
     | Default Database Connection Name
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -58,16 +58,15 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
-            'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require',
         ],
 
         'sqlsrv' => [
