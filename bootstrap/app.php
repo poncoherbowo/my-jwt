d5a903b56948ec4e5993c9d13f9dab83012a3f0b
Ponco Herbowo <ponco.94@gmail.com>
2019-08-12 14:04:47 +0700

    make it better

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,4 +1,7 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST,GET');
+header('Access-Control-Allow-Headers: *');
 
 /*
 |--------------------------------------------------------------------------
